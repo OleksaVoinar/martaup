0e76e2d4f47aa485a1c1bc99ac61b2b448cae654
AlexVoina <olexsavoinar@gmail.com>
2020-10-21 13:56:48 +0300

    21.10

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,14 @@
 $(function(){
 
+  $('.header-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    arrows:false,
+    dotsClass:'header-dots'
+  });
 
-
-  
 });
+
